5b714ef7b9cb75d553184f39416ec40ee1f25968
rickyVvv <48536558+rickyVvv@users.noreply.github.com>
2021-12-14 17:49:34 -0600

    Update cicd-pipeline.tf

--- cicd-pipeline.tf
+++ cicd-pipeline.tf
@@ -69,7 +69,7 @@ resource "aws_codepipeline" "cicd_pipeline" {
             version = "1"
             output_artifacts = ["tf-code"]
             configuration = {
-                FullRepositoryId = "davoclock/aws-cicd-pipeline"
+                FullRepositoryId = "rickyVvv/AWS-CICD-pipeline"
                 BranchName   = "master"
                 ConnectionArn = var.codestar_connector_credentials
                 OutputArtifactFormat = "CODE_ZIP"
